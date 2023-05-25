4483bc11f6b64e8e13ec35c776679ec522c0cd8d
dimuthu1992 <129645576+dimuthu1992@users.noreply.github.com>
2023-05-25 23:50:46 +0530

    unit-buttons

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const apiKey="f3373d156860410a8d683135231405";
 const apiurl="http://api.weatherapi.com/v1/current.json?key=f3373d156860410a8d683135231405&q=";
 
+checkCurrentWeather("Kalutara");
 function sendCurrentLocation(){
     var searchedLocation=document.getElementById("search-bar").value;
     checkCurrentWeather(searchedLocation);
@@ -24,6 +25,8 @@ async function checkCurrentWeather(cityName){
         document.getElementById("invalid-city").innerHTML="Invalid city name..!";
         document.getElementById("invalid-city").style.backgroundColor = "red";
     }else{
+        document.getElementById("celcius-button").innerHTML="°C";
+        document.getElementById("speed-unit-button").innerHTML="kph";
         document.getElementById("invalid-city").innerHTML="Plan your day";
         document.getElementById("invalid-city").style.removeProperty("background-color");
         var data=await response.json();
@@ -33,16 +36,18 @@ async function checkCurrentWeather(cityName){
         document.getElementById("ctemp").innerHTML=temp;
         document.getElementById("status").innerHTML=data.current.condition.text;
         document.getElementById("humid-value").innerHTML=data.current.humidity+"%";
-        document.getElementById("wind-speed-value").innerHTML=Math.round(data.current.wind_kph);
+        document.getElementById("wind-speed-value").innerHTML=data.current.wind_kph;
         document.getElementById("statusImage").src=weatherCode;
+        document.getElementById("date-time").innerHTML=data.current.last_updated;
 
         for(var i=1;i<4;i++){
             document.getElementById("day"+i).innerHTML=data.forecast.forecastday[i].date;
-            document.getElementById("f"+i+"temperature").innerHTML=Math.round(data.forecast.forecastday[i].day.mintemp_c)+"°|"+Math.round(data.forecast.forecastday[i].day.maxtemp_c)+"°";
+            document.getElementById("f"+i+"temperature").innerHTML=Math.round(data.forecast.forecastday[i].day.mintemp_c)+"°";
+            document.getElementById("f"+i+"temperaturef").innerHTML=Math.round(data.forecast.forecastday[i].day.maxtemp_c)+"°";
             document.getElementById("f"+i+"status").innerHTML=data.forecast.forecastday[i].day.condition.text;
             document.getElementById("f"+i+"hum-value").innerHTML=data.forecast.forecastday[i].day.avghumidity+"%";
             document.getElementById("f"+i+"weather-image").src=data.forecast.forecastday[i].day.condition.icon;
-            document.getElementById("f"+i+"speed-value").innerHTML=data.forecast.forecastday[i].day.maxwind_kph+"km/h";
+            document.getElementById("f"+i+"speed-value").innerHTML=data.forecast.forecastday[i].day.maxwind_kph;
         }
         for(var i=0;i<24;i++){
             document.getElementById("n"+(i+1)+"temp").innerHTML=Math.round(data.forecast.forecastday[0].hour[i].temp_c)+"°";
@@ -53,7 +58,88 @@ async function checkCurrentWeather(cityName){
     }  
 
 } 
- function checkHistory(cityName){
+function convertToFaranhite(tempc){
+    var tempInC=tempc*9/5+32;
+    return Math.round(tempInC);
+}
+function convertToCelcious(tempf){
+    var tempInF=(tempf-32)*5/9;
+    return Math.round(tempInF);
+}
+function convertTokph(mph){
+    var kph=mph*1.60934;
+    return kph.toFixed(1);
+}
+function convertTomph(kph){
+    var mph=kph*0.621371;
+    return mph.toFixed(1);
+}
+const kphButton=document.getElementById("speed-unit-button");
+kphButton.addEventListener('click',async()=>{
+    if(document.getElementById("speed-unit-button").innerHTML=="kph"){
+        document.getElementById("speed-unit-button").innerHTML="mph";
+        document.getElementById("wind-speed-value").innerHTML=convertTomph(document.getElementById("wind-speed-value").innerHTML);
+        console.log(document.getElementById("f1speed-value"));
+        for(var i=1;1<4;i++){
+            document.getElementById("f"+i+"speed-value").innerHTML=convertTomph(parseFloat(document.getElementById("f"+i+"speed-value").innerHTML));
+        }
+
+        for(var i=1;i<8;i++){
+            document.getElementById("h"+i+"speed-value").innerHTML=convertTomph(parseFloat(document.getElementById("h"+i+"speed-value").innerHTML));
+        }
+    }else if(document.getElementById("speed-unit-button").innerHTML=="mph"){
+        document.getElementById("speed-unit-button").innerHTML="kph";
+        document.getElementById("wind-speed-value").innerHTML=convertTokph(parseFloat(document.getElementById("wind-speed-value").innerHTML));
+
+        for(var i=1;1<4;i++){
+            document.getElementById("f"+i+"speed-value").innerHTML=convertTokph(parseFloat(document.getElementById("f"+i+"speed-value").innerHTML));
+        }
+        console.log(document.getElementById("f"+i+"speed-value").innerHTML);
+        for(var i=1;i<8;i++){
+            document.getElementById("h"+i+"speed-value").innerHTML=convertTokph(parseFloat(document.getElementById("h"+i+"speed-value").innerHTML));
+        }
+    }
+
+});
+
+const unitButton=document.getElementById("celcius-button");
+unitButton.addEventListener('click',async()=>{
+var unit=document.getElementById("celcius-button").innerHTML;
+if(unit=="°C"){
+    document.getElementById("celcius-button").innerHTML="°F";
+    document.getElementById("ctemp").innerHTML=convertToFaranhite(parseInt(document.getElementById("ctemp").innerHTML));
+
+    for(var i=1;i<8;i++){
+        document.getElementById("h"+i+"temperature").innerHTML=convertToFaranhite(parseInt(document.getElementById("h"+i+"temperature").innerHTML))+"°";
+        document.getElementById("h"+i+"temperaturef").innerHTML=convertToFaranhite(parseInt(document.getElementById("h"+i+"temperaturef").innerHTML))+"°";
+    }
+
+    for(var i=1;i<4;i++){        
+        document.getElementById("f"+i+"temperature").innerHTML=convertToFaranhite(parseInt(document.getElementById("f"+i+"temperature").innerHTML))+"°";
+        document.getElementById("f"+i+"temperaturef").innerHTML=convertToFaranhite(parseInt(document.getElementById("f"+i+"temperaturef").innerHTML))+"°";
+    }   
+    for(var i=1;i<25;i++){
+        document.getElementById("n"+i+"temp").innerHTML=convertToFaranhite(parseInt(document.getElementById("n"+i+"temp").innerHTML))+"°";
+    }
+}else if(unit=="°F"){
+    document.getElementById("celcius-button").innerHTML="°C";
+    document.getElementById("ctemp").innerHTML=convertToCelcious(parseInt(document.getElementById("ctemp").innerHTML));
+    
+    for(var i=1;i<4;i++){
+        document.getElementById("f"+i+"temperature").innerHTML=convertToCelcious(parseInt(document.getElementById("f"+i+"temperature").innerHTML))+"°";
+        document.getElementById("f"+i+"temperaturef").innerHTML=convertToCelcious(parseInt(document.getElementById("f"+i+"temperaturef").innerHTML))+"°";
+        
+    }
+    for(var i=1;i<8;i++){
+        document.getElementById("h"+i+"temperature").innerHTML=convertToCelcious(parseInt(document.getElementById("h"+i+"temperature").innerHTML))+"°";
+        document.getElementById("h"+i+"temperaturef").innerHTML=convertToCelcious(parseInt(document.getElementById("h"+i+"temperaturef").innerHTML))+"°";
+    }
+    for(var i=1;i<25;i++){
+        document.getElementById("n"+i+"temp").innerHTML=convertToCelcious(parseInt(document.getElementById("n"+i+"temp").innerHTML))+"°";
+    }
+}
+ });
+function checkHistory(cityName){
     for(var i=0;i<7;i++){
         var today = new Date();
         today.setDate(today.getDate() -(i+1));
@@ -62,30 +148,32 @@ async function checkCurrentWeather(cityName){
         var yyyy = today.getFullYear();
 
         today = yyyy+'/'+mm+'/'+dd  ;
-        var id="h"+(i+1)+"temperature";
+        var idc="h"+(i+1)+"temperature";
+        var idf="h"+(i+1)+"temperaturef";
         var hstatus ="h"+(i+1)+"status";
         var wimage="h"+(i+1)+"weather-image";
         var hhumid="h"+(i+1)+"hum-value";
         var wspeed="h"+(i+1)+"speed-value"
 
-        fetchLoop(id,hstatus,wimage,hhumid,wspeed,cityName,today);
+        fetchLoop(idc,idf,hstatus,wimage,hhumid,wspeed,cityName,today);
         
 
       document.getElementById("hday"+(i+1)).innerHTML=today;
     }
 }
-function fetchLoop(id,hstatus,wimage,hhumid,wspeed,cityName,today){
+function fetchLoop(idc,idf,hstatus,wimage,hhumid,wspeed,cityName,today){
     fetch(`http://api.weatherapi.com/v1/history.json?key=f3373d156860410a8d683135231405&q=${cityName}&dt=${today}`)
     .then(response=>response.json())
     .then(json=>{
         var maxtemp=Math.round(json.forecast.forecastday[0].day.maxtemp_c);
         var mintemp=Math.round(json.forecast.forecastday[0].day.mintemp_c);
         console.log(mintemp);
-        document.getElementById(id).innerHTML=mintemp+"°|"+maxtemp+"°";
+        document.getElementById(idc).innerHTML=mintemp+"°";
+        document.getElementById(idf).innerHTML=maxtemp+"°";
         document.getElementById(hstatus).innerHTML=json.forecast.forecastday[0].day.condition.text;
         document.getElementById(wimage).src=json.forecast.forecastday[0].day.condition.icon;
         document.getElementById(hhumid).innerHTML=json.forecast.forecastday[0].day.avghumidity+"%";
-        document.getElementById(wspeed).innerHTML=json.forecast.forecastday[0].day.maxwind_kph+"km/h";
+        document.getElementById(wspeed).innerHTML=json.forecast.forecastday[0].day.maxwind_kph;
     });
 }
  async function checkLocationWeather(lang,longt){
@@ -97,31 +185,6 @@ function fetchLoop(id,hstatus,wimage,hhumid,wspeed,cityName,today){
         document.getElementById("invalid-city").innerHTML="Plan your day";
         document.getElementById("invalid-city").style.removeProperty("background-color");
         var data=await response.json();
-        var weatherCode=data.current.condition.icon;
-        var temp=Math.round(data.current.temp_c);
-        document.getElementById("city-name").innerHTML=data.location.name;
-        document.getElementById("ctemp").innerHTML=temp;
-        document.getElementById("status").innerHTML=data.current.condition.text;
-        document.getElementById("humid-value").innerHTML=data.current.humidity+"%";
-        document.getElementById("wind-speed-value").innerHTML=Math.round(data.current.wind_kph);
-        document.getElementById("statusImage").src=weatherCode;
-
-        for(var i=1;i<4;i++){
-            document.getElementById("day"+i).innerHTML=data.forecast.forecastday[i].date;
-            document.getElementById("f"+i+"temperature").innerHTML=Math.round(data.forecast.forecastday[i].day.mintemp_c)+"°|"+Math.round(data.forecast.forecastday[i].day.maxtemp_c)+"°";
-            document.getElementById("f"+i+"status").innerHTML=data.forecast.forecastday[i].day.condition.text;
-            document.getElementById("f"+i+"hum-value").innerHTML=data.forecast.forecastday[i].day.avghumidity+"%";
-            document.getElementById("f"+i+"weather-image").src=data.forecast.forecastday[i].day.condition.icon;
-            document.getElementById("f"+i+"speed-value").innerHTML=data.forecast.forecastday[i].day.maxwind_kph+"km/h";
-        }
-        for(var i=0;i<24;i++){
-            document.getElementById("n"+(i+1)+"temp").innerHTML=Math.round(data.forecast.forecastday[0].hour[i].temp_c)+"°";
-            document.getElementById("n"+(i+1)+"image").src=data.forecast.forecastday[0].hour[i].condition.icon;
-            document.getElementById("n"+(i+1)+"time").innerHTML=i<10?"0"+i+":00":i+":00";
-        }
-        checkHistory(data.location.name);
-    }  
-
+        checkCurrentWeather(data.location.name);
+    }
 } 
-
-checkCurrentWeather("Colombo");
